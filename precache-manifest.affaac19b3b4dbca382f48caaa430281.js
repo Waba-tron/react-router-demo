c9fe9f40384324aa1248072d0e4fee18d01c77b7
Wahab Rehman <wahab_rehman1256@outlook.com>
2020-05-06 21:01:09 +0100

    Updates

---
+++ precache-manifest.affaac19b3b4dbca382f48caaa430281.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45afb5facd1dac4ccfbf50839ec64d6c",
+    "url": "/react-router-demo/index.html"
+  },
+  {
+    "revision": "3aacc34badab50625838",
+    "url": "/react-router-demo/static/css/main.2b28c9f6.chunk.css"
+  },
+  {
+    "revision": "6b87020ddcaaa699ea83",
+    "url": "/react-router-demo/static/js/2.57a5d7d9.chunk.js"
+  },
+  {
+    "revision": "3aacc34badab50625838",
+    "url": "/react-router-demo/static/js/main.02651de0.chunk.js"
+  },
+  {
+    "revision": "00c954839df506636475",
+    "url": "/react-router-demo/static/js/runtime~main.67d6bcf9.js"
+  }
+]);
